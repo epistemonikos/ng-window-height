b4b63642a2f31b87afac91c41af40b82e64b43f6
Felipe Valverde <felipe.valverde.campos@gmail.com>
2014-07-28 18:13:23 -0400

    fix: overflow-y

--- angular-window-height.js
+++ angular-window-height.js
@@ -6,26 +6,27 @@ angular.module("window-height", [])
             bottom_offset: '@bottomOffset'
         },
         link: function (scope, element, attrs) {
-            scope.bottom_offset = isNaN(scope.bottom_offset) ? 0 : parseInt(scope.bottom_offset);
-            scope.footer = scope.footer || 'footer';
-            scope.footer_elem = angular.element('.footer').length ? angular.element('.footer') : angular.element('#footer');
-            element.css('overflow','hidden');
+            scope.footer_elem = angular.element(scope.footer || '#footer');
+            element.css('overflow-x', 'hidden');
+            element.css('overflow-y', 'scroll');
             var _window = angular.element($window);
+            var _bottom_offset = isNaN(scope.bottom_offset) ? 0 : parseInt(scope.bottom_offset);
 
             scope.setHeight = function () {
                 var new_height = _window.height();
                 new_height -= scope.footer_elem.outerHeight(true); // footer outer height
                 new_height -= element.offset().top; // element top px
-                new_height -= scope.bottom_offset; // extra bottom offset
+                new_height -= _bottom_offset; // extra bottom offset
                 element.height(new_height+'px');
             };
 
             /* before bind rezise ! */
             scope.setHeight();
 
+            /* keep size when resize window */
             _window.bind('resize', function () {
                 scope.setHeight();
             });
         }
-    }
+    };
 });
