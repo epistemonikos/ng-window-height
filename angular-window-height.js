9d78edbfb3b1097a2484e529b173cc41867c3140
Felipe Valverde <felipe.valverde.campos@gmail.com>
2014-07-31 12:38:35 -0400

    IE9 overflow fix: notify to all elements

--- angular-window-height.js
+++ angular-window-height.js
@@ -1,5 +1,5 @@
 angular.module("ngWindowHeight", [])
-.directive('windowHeight', function ($window) {
+.directive('windowHeight', function ($window, $timeout) {
     return {
         scope: {
             footer: '@',
@@ -9,23 +9,32 @@ angular.module("ngWindowHeight", [])
             scope.footer_elem = angular.element(scope.footer || '#footer');
             element.css('overflow-x', 'hidden');
             element.css('overflow-y', 'auto');
-            var _window = angular.element($window);
-            var _bottom_offset = !scope.bottom_offset || isNaN(scope.bottom_offset) ? 0 : parseInt(scope.bottom_offset);
+            var _bottom_offset = isNaN(scope.bottom_offset) || !scope.bottom_offset ? 0 : parseInt(scope.bottom_offset);
 
             scope.setHeight = function () {
-                var new_height = _window.height();
+                var new_height = angular.element($window).height();
                 new_height -= scope.footer_elem.outerHeight(true); // footer outer height
                 new_height -= element.offset().top; // element top px
                 new_height -= _bottom_offset; // extra bottom offset
                 element.height(new_height+'px');
             };
 
-            /* before bind rezise ! */
-            scope.setHeight();
+            /* first height before bind rezise ! */
+            $timeout(function () {
+                scope.setHeight();
+            });
 
             /* keep size when resize window */
-            _window.bind('resize', scope.setHeight);
+            angular.element($window).bind('resize', scope.setHeight);
             angular.element('body').on('OverflowEvent' in window ? 'overflowchanged' : 'overflow', scope.setHeight);
+
+           /* IE9 overflow fix: notify to all elements */
+            scope.$on('MOUSEENTER', function () {
+                scope.setHeight();
+            });
+            element.bind('mouseenter', function () {
+                scope.$root.$broadcast('MOUSEENTER');
+            });
         }
     };
 });
